a1aa2cb19283e5c0e69c515b4ce3980a3b85d6ac
nalexanderdev <nalexanderdev@gmail.com>
2017-06-21 23:15:00 +0100

    Initial commit

---
+++ app/src/main/java/com/example/benitkibabu/firebaseexample/activities/PostListViewAdapter.java
@@ -0,0 +1,17 @@
+package com.example.benitkibabu.firebaseexample.activities;
+
+import com.example.benitkibabu.firebaseexample.models.Post;
+
+import java.util.List;
+
+/**
+ * Created by user on 21-Jun-17.
+ */
+
+class PostListViewAdapter {
+    public PostListViewAdapter(ThreadLocal<PostListViewAdapter> threadLocal, int post_item, List<Post> postList) {
+    }
+
+    public void updateList(List<Post> myPosts) {
+    }
+}
